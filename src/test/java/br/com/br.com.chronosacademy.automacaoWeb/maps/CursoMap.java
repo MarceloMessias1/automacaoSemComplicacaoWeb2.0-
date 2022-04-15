a51eabcf2671d7dbbb832ddc577facc7fbaf0633
Marcelo <cheroxmiller@gmail.com>
2022-04-15 10:27:34 -0300

    aula 10 pageFactory

---
+++ src/test/java/br/com/br.com.chronosacademy.automacaoWeb/maps/CursoMap.java
@@ -0,0 +1,10 @@
+package br.com.br.com.chronosacademy.automacaoWeb.maps;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CursoMap {
+    @FindBy(css = "div.text-center>div.large-h1")
+    public WebElement txtTitulo;
+
+}
--- src/test/java/br/com/br.com.chronosacademy.automacaoWeb/pages/CursoPage.java
+++ src/test/java/br/com/br.com.chronosacademy.automacaoWeb/pages/CursoPage.java
@@ -1,22 +1,24 @@
 package br.com.br.com.chronosacademy.automacaoWeb.pages;
 
+import br.com.br.com.chronosacademy.automacaoWeb.maps.CursoMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CursoPage {
 
     WebDriver driver;
+    CursoMap cursoMap;
 
-    public CursoPage(WebDriver driver) {
+    public CursoPage(WebDriver driver)
+    {
         this.driver = driver;
+        cursoMap = new CursoMap();
+        PageFactory.initElements(driver, cursoMap);
     }
 
     public String getTitulo2() {
-        String cssTítulo = "div.text-center>div.large-h1";
-        WebElement txtTitulo = driver.findElement(By.cssSelector(cssTítulo));
-        String titulo = txtTitulo.getText();
-        return titulo;
-    }
-
+        return cursoMap.txtTitulo.getText();
+            }
 }
