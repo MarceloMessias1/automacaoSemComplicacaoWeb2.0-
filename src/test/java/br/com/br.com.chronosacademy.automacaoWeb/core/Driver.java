0f64815ddf535dd53e79a8ad405bc1de98a40388
Marcelo <cheroxmiller@gmail.com>
2022-05-24 11:23:25 -0300

    aula 17 try catch e excecao

--- src/test/java/br/com/br.com.chronosacademy.automacaoWeb/core/Driver.java
+++ src/test/java/br/com/br.com.chronosacademy.automacaoWeb/core/Driver.java
@@ -1,5 +1,6 @@
 package br.com.br.com.chronosacademy.automacaoWeb.core;
 
+import io.cucumber.messages.Messages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,17 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static java.time.Duration.ofSeconds;
+
 public class Driver {
     private static WebDriver driver;
     private static WebDriverWait wait;
 
-        public Driver(String navegador) {
+    public Driver(String navegador){
 
             switch (navegador) {
                 case "chrome":
@@ -41,23 +43,21 @@ public class Driver {
                     driver = new ChromeDriver();
                     break;
             }
-            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait = new WebDriverWait(driver,Duration.ofSeconds(10));
             driver.manage().window().maximize();
         }
 
-        public static void  visibilityOf(WebElement element){
-            wait.until(ExpectedConditions.visibilityOf(element));
-        }
-        public static void invisibilityOf(WebElement element){
-            wait.until(ExpectedConditions.invisibilityOf(element));
-        }
-            public static WebDriver getDriver(){
-                return driver;
-            }
+    public static void visibilityOf(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void invisibilityOf(WebElement element){
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
 
 
-        }
-
-
-
-
+}
--- src/test/java/br/com/br.com.chronosacademy.automacaoWeb/runner/RunnerTest.java
+++ src/test/java/br/com/br.com.chronosacademy.automacaoWeb/runner/RunnerTest.java
@@ -7,13 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/",
-        tags = "",
-        glue = "src/test/java/br/com/chronosacademy/automacaoWeb/steps",
-        plugin = {"json:target/reports/CucumberReports.json","pretty"},
+        tags = "@fecharModal",
+        glue = "br.com.br.com.chronosacademy.automacaoWeb.steps",
+        plugin = {"json:target/reports/CucumberReports.json", "pretty"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class RunnerTest {
 
-
-
 }
+
+
+
